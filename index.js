5845f8d3ffb97c26e7d45353421aff95ee6025ff
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-18 23:19:23 -0600

    project title/description

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const inquirer = require ('inquirer');
 
-const promptUser = () => {
+const readmeInput = () => {
+    console.log(`
+=================
+Welcome!! Lets make your readme simple/quick!
+=================
+`);
+
     return inquirer.prompt([
         {
+            // get the title
             type: 'input',
             name: 'name',
-            message: "What is your name? (Required)",
-        }
+            message: "What is your project title? (Required)",
+        },
+        {
+            // get the description
+            type: 'input',
+            name: 'description',
+            message: 'Write a brief description of your project. (required)',
+        },
+        {
+            // get instalation instructions - needs more options (a confirm? then steps after 1.2.3.etc)
+            type: 'confirm',
+            name: 'installation',
+            message: 'Does your project require installation?',
+        },
     ])
 }
 
-promptUser()
-.then(promptUser => {
+readmeInput()
+.then(readmeInput => {
     // setting the variable does not work? -> const {name} = promptUser.name;
-    console.log(promptUser.name);
+    // name of project
+    console.log(readmeInput.name);
+    // description
+    console.log(readmeInput.description);
+    // installation
+    console.log(readmeInput.installation);
 });
