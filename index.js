748f41ea3a62e229f4d584d0f1ac003e317f26ef
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-20 20:47:50 -0600

    initial inquirer questions

--- index.js
+++ index.js
@@ -26,6 +26,48 @@ Welcome!! Lets make your readme simple/quick!
             name: 'installation',
             message: 'Does your project require installation?',
         },
+        {
+            // get usage description
+            type: 'input',
+            name: 'usage',
+            message: 'Please describe the usage of this project.',
+        },
+        {
+            // get license preference
+            type: 'input',
+            name: 'license',
+            message: 'Which license would you like to use?',
+        },
+        {
+            // get contributors preference
+            type: 'confirm',
+            name: 'confirmContributors',
+            message: 'do you want to add a contributor?',
+        },
+        {
+            // get test preference
+            type: 'confirm',
+            name: 'testConfirm',
+            message: 'Do you want to add test?',
+        },
+        {
+            // get github username
+            type: 'input',
+            name: 'username',
+            message: 'Please enter your GitHub username',
+        },
+        {
+            // get email
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email for contact.',
+        },
+        {
+            // get contact instructions
+            type: 'input',
+            name: 'contactInstructions',
+            message: 'Please enter instructions on how to reach you for questions',
+        }
     ])
 }
 
@@ -38,4 +80,18 @@ readmeInput()
     console.log(readmeInput.description);
     // installation
     console.log(readmeInput.installation);
+    // usage
+    console.log(readmeInput.usage);
+    // license
+    console.log(readmeInput.license);
+    // contributors
+    console.log(readmeInput.confirmContributors);
+    // tests
+    console.log(readmeInput.testConfirm);
+    // questions section - username
+    console.log(readmeInput.username);
+    // email
+    console.log(readmeInput.email);
+    // contact instructions
+    console.log(readmeInput.contactInstructions);
 });
